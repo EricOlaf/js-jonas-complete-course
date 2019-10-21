6c4c9f1d1c4e01dc3d7f3ca36b716d7de40559d8
Eric Olaveson <olavesoneric@gmail.com>
2019-10-21 17:11:47 -0500

    done w js challenge 7

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -5,7 +5,8 @@ class Element{
     }
 }
 
-class Parks extends Element{
+//PARKS
+class Park extends Element{
     constructor(name, year, trees, parkArea,){
         super(name, year);
         this.trees = trees;
@@ -16,13 +17,14 @@ class Parks extends Element{
     }
 }
 
-class Streets extends Element{
+//STREETS
+class Street extends Element{
     constructor(name, year, streetLength){
         super(name, year);
         this.streetLength = streetLength;
     }
     streetClassFunc(){
-        streetInternalFunc(streetClass){
+        let streetInternalFunc = (streetClass) => {
             console.log(`${this.name}, built in ${this.year} is a ${streetClass} street.`)
         }
         switch(true){
@@ -45,5 +47,48 @@ class Streets extends Element{
                 streetInternalFunc('normal');
         }
     }
-
 }
+
+//FUNCTIONS
+
+function avgParkAge(arr){
+    let totalAge = 0
+    arr.forEach(x=>{
+        totalAge += new Date().getFullYear() - x.year
+    })
+    return totalAge / arr.length 
+}
+
+function thousandTrees(arr){
+    let ans = arr.filter(x=>x.trees >= 1000)
+    return ans[0].name
+}
+
+function streetLengthAvg(arr){
+    let totalLength = 0;
+    arr.forEach(x=> totalLength += x.streetLength)
+    return(totalLength, totalLength/arr.length)
+}
+
+
+
+let allParks = [
+    new Park("Rigby Park", 1974, 382, 690),
+    new Park("Rigby Lake Park", 1994, 802, 801),
+    new Park("Labelle Park", 1919, 1112, 880)
+]
+
+let allStreets = [
+    new Street("Main Street", 1872, 440),
+    new Street("456 Nth Street", 1999, 1200),
+    new Street("Jefferson Street", 1953, 3269),
+    new Street("Menan Road", 1893, 1886)
+]
+
+console.log(avgParkAge(allParks))
+console.log(allParks[0].treeDensity())
+console.log(avgParkAge(allParks))
+console.log(thousandTrees(allParks))
+console.log(streetLengthAvg(allStreets))
+allStreets[1].streetClassFunc()
+allStreets[0].streetClassFunc()
