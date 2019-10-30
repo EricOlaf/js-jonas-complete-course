490f8a602af7af77206a60c7c66d63b725b20d5e
Eric Olaveson <olavesoneric@gmail.com>
2019-10-29 20:50:22 -0500

    done with initial setup on listeners

--- 6-budgety/starter/app2.js
+++ 6-budgety/starter/app2.js
@@ -3,30 +3,57 @@
 budgetModule = (function(){
 
     return{
-        test: function(){
-            console.log("budget module works")
-        }
+       
     }
 })()
 
 uiModule = (function(){
+    const DOMstrings = {
+        //setup DOM strings
+        addType: '.add__type',
+        addDesc: '.add__description',
+        addValue: '.add__value',
+        addBtn: '.add__btn'
+    }
 
     return{
-        test: function(){
-            console.log("ui module works")
+        inputValues:function() {
+            //return the values from the DOM inputs.
+            return{
+                inType: document.querySelector(DOMstrings.addType).value,
+                inDesc: document.querySelector(DOMstrings.addDesc).value,
+                inValue: parseFloat(document.querySelector(DOMstrings.addValue).value)
+            }
+        },
+        getDomStr: function(){
+            return DOMstrings
         }
+       
     }
 })()
 
 controllerModule = (function(bdMod, uiMod){
+   
+    function setupListeners (){
+         //Setup the click listener on the button
+        const DOMstrings = uiMod.getDomStr();
+        console.log(DOMstrings)
+        document.querySelector(DOMstrings.addBtn).addEventListener('click', addInputItems)
+    }
 
+    function addInputItems(){
+        //get and store the input values then send them to the budget module to perform calculations, then be able to update the ui with the new values.
+        var input = uiMod.inputValues()
+        if(input.inDesc && input.inValue){
+            console.log(input);
+        }
+    }
 
     return{
-        test: function(){
-           bdMod.test();
-           uiMod.test();
-        }
+       init:function(){
+        setupListeners()
+       }
     }
 })(budgetModule, uiModule)
 
-controllerModule.test()
+controllerModule.init()
