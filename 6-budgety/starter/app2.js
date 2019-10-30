1b94c86f29db087aac5ea182bb1740f5bab2c947
Eric Olaveson <olavesoneric@gmail.com>
2019-10-29 19:54:10 -0500

    modules linked

--- 6-budgety/starter/app2.js
+++ 6-budgety/starter/app2.js
@@ -1,1 +1,32 @@
 //Remake of app.js on my own to test my skills.
+
+budgetModule = (function(){
+
+    return{
+        test: function(){
+            console.log("budget module works")
+        }
+    }
+})()
+
+uiModule = (function(){
+
+    return{
+        test: function(){
+            console.log("ui module works")
+        }
+    }
+})()
+
+controllerModule = (function(bdMod, uiMod){
+
+
+    return{
+        test: function(){
+           bdMod.test();
+           uiMod.test();
+        }
+    }
+})(budgetModule, uiModule)
+
+controllerModule.test()
