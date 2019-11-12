ae3176e13695a75759662046765fe3cf1139288b
Eric Olaveson <olavesoneric@gmail.com>
2019-11-12 10:54:14 -0600

    recipe class with prototypal methods

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
 import {elements, elementStrings, renderLoader, stopLoader} from './views/base';
 //only search for pizza bacon and broccoli.
@@ -12,6 +13,8 @@ import {elements, elementStrings, renderLoader, stopLoader} from './views/base';
 */
 
 const state = {}
+
+/*SEARCH CONTROLLER*/
 const controlSearch = async () => {
     //1 Get query from view
     const query = searchView.getInput();
@@ -28,8 +31,9 @@ const controlSearch = async () => {
         await state.search.getResults();
 
         //5 Render the results to the ui
-        stopLoader(elementStrings.loaderClass)
-        searchView.renderResults(state.search.result)
+        stopLoader(elementStrings.loaderClass);
+        searchView.renderResults(state.search.result);
+
     }
 }
 elements.searchForm.addEventListener('submit', e => {
@@ -45,3 +49,16 @@ elements.searchResPages.addEventListener('click', e=>{
         searchView.renderResults(state.search.result, goToPage)
     }
 })
+
+
+/*RECIPE CONTROLLER*/
+
+async function myFunc(){
+    const r = new Recipe(35120)
+    await r.getRecipe();
+    r.calcTime();
+    r.calcServings();
+    console.log(r)
+}
+
+myFunc();
--- 9-forkify/starter/src/js/models/Recipe.js
+++ 9-forkify/starter/src/js/models/Recipe.js
@@ -1,1 +1,33 @@
-//const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+//const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`); 35120
+
+import axios from 'axios'
+
+export default class Recipe{
+    constructor(id){
+        this.id = id
+    }
+    async getRecipe(){
+        try{
+            const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+            this.title = res.data.recipe.title;
+            this.imageUrl = res.data.recipe.image_url;
+            this.publisher = res.data.recipe.publisher;
+            this.ingredients = res.data.recipe.ingredients;
+            this.sourceUrl = res.data.recipe.source_url;
+        }catch(err){
+            console.log(err)
+            alert(err)
+        }
+    }
+
+    calcTime(){
+        //assuming we need 15 minutes for every 3 ingredients. Not real!!!
+        const numIng = this.ingredients.length;
+        const periods = numIng/3;
+        this.time = periods * 15;
+    }
+
+    calcServings(){
+        this.servings = 4;
+    }
+}
