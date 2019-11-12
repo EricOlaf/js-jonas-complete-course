5779648064cfdac3b91c0b799d344ae89516e21b
Eric Olaveson <olavesoneric@gmail.com>
2019-11-11 18:02:52 -0600

    render api list components

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,1 +1,36 @@
-import str from "./models/Search";
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
+//only search for pizza bacon and broccoli.
+
+/*
+***Global State***
+- search object
+- current recipe object
+- shopping list object
+- liked recipes
+*/
+
+const state = {}
+const controlSearch = async () => {
+    //1 Get query from view
+    const query = searchView.getInput();
+    if(query){
+        //2 make a new object with the query
+        state.search = new Search(query);
+
+        //3 Prep the ui for the results
+        searchView.clearInput();
+        searchView.clearResults();
+
+        //4 Search for the recipes
+        await state.search.getResults();
+
+        //5 Render the results to the ui
+        searchView.renderResults(state.search.result)
+    }
+}
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
