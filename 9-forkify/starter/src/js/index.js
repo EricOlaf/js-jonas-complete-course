54912fb7c46829017fa8f2c0982e1abead54c160
Eric Olaveson <olavesoneric@gmail.com>
2019-11-13 16:06:43 -0600

    add/remove highlight for recipe cards

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -70,6 +70,9 @@ const controlRecipe = async () => {
         recipeView.clearRecipe()
         renderLoader(elements.recipeDiv);
 
+        //highlight selected
+        if(state.search){searchView.highlightedSelected(id)}
+
         //Create new recipe object
         state.recipe = new Recipe(id)
 
